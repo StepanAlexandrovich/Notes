a6f9339d289733aeb7a8cc68c22689a67e55ae47
stepan <fisenko.stepan@mail.ru>
2022-06-14 12:48:59 +0300

    lesson_11_4

--- app/src/main/java/java/android/notes/Time.java
+++ app/src/main/java/java/android/notes/Time.java
@@ -1,23 +1,22 @@
 package java.android.notes;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 public class Time {
-    //private static Date date = new Date();
-    //public static Date getDate() { return date; }
+    public static Calendar calendar = Calendar.getInstance();
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MMMM.yyyy");
 
-    private static Calendar calendar = Calendar.getInstance();
-    private static int[] dateYMD = {Calendar.DAY_OF_MONTH,Calendar.MONTH,Calendar.YEAR};
+    public static void setDay   (int day)   { calendar.set(Calendar.DAY_OF_MONTH,day); }
+    public static void setMonth (int month) { calendar.set(Calendar.MONTH,month);      }
+    public static void setYear  (int year)  { calendar.set(Calendar.YEAR,year);        }
 
-    public static void setDay(int i){ dateYMD[0] = i; }
-    public static void setMonth(int i){ dateYMD[1] = i; }
-    public static void setYear(int i){ dateYMD[2] = i; }
+    public static int getDay(){ return calendar.get(Calendar.DAY_OF_MONTH); }
+    public static int getMonth(){ return calendar.get(Calendar.MONTH); }
+    public static int getYear(){ return calendar.get(Calendar.YEAR); }
 
-    public static int getDay(){ return dateYMD[0]; }
-    public static int getMonth(){ return dateYMD[1]; }
-    public static int getYear(){ return dateYMD[2]; }
-
-    public static String getDateYMD(){ return ""+dateYMD[0]+" "+dateYMD[1]+" "+dateYMD[2]; }
+    public static String getDateYMD(){
+        return dateFormat.format(calendar.getTime());
+    }
 
 }
